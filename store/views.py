624303731b9a6cec9423e7abf94109096b89a092
mohammad <aliakbariiimohammad@gmail.com>
2021-10-05 15:11:12 +0330

    changing setting for media and statics nd prevent admin panel

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Category,Product
 # Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    return  render(request,'store/home.html',context={
+        'products':products
+    })
+
+
