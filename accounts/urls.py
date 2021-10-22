9e0462fa3fb48206d4db5a5c9154590a5a8ec918
mohammad <aliakbariiimohammad@gmail.com>
2021-10-22 12:07:54 +0330

    solved bug of login staff

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/',views.sign_up,name='sign_up'),
     path('login/',views.login_customer,name='login_customer'),
-    path('login/', views.login_staff, name='login_staff'),
+    # path('login/admin/', views.login_staff, name='login_staff'),
     path('logout/', views.logOut, name='logout'),
     path('edit_profile/',views.edit_profile,name='edit_profile'),
 ]
--- eccommerce_web/urls.py
+++ eccommerce_web/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import logOut
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls',namespace='store')),
